544afd2195bd00041ce6051cdecbad13437365f7
slink42 <13059870+slink42@users.noreply.github.com>
2021-12-26 11:34:07 +1100

    Added py install requirement

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Transmission Cleaner',
+      version='0.1',
+      description='Transmission Batch Cleaning Utilities',
+      author='Stuart Thomas',
+      author_email='13059870+slink42@users.noreply.github.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=find_packages(),
+     )
